a67d6d56634385d936de6708a97f7ff4df62cf76
usamaadev <usamaadev@gmail.com>
2023-10-31 16:57:03 +0500

    [fix] CORS added

--- medusa-config.js
+++ medusa-config.js
@@ -23,10 +23,13 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+  process.env.ADMIN_CORS ||
+  "https://medusajs-admin-panel-dashboard-brand.vercel.app, http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS =
+  process.env.STORE_CORS ||
+  "https://medusajs-storefront-starter.vercel.app, http://localhost:8000";
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
